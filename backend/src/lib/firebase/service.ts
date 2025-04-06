b188fe62ab1ebc0f933764a73f56285266256476
NestiaDev-id <yohanesdevano90@gmail.com>
2025-04-06 22:06:56 +0700

    update service database

--- backend/src/lib/firebase/service.ts
+++ backend/src/lib/firebase/service.ts
@@ -1,6 +1,16 @@
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  getFirestore,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 
 import app from "./init";
+import bcrypt from "bcrypt";
 
 const firestore = getFirestore(app);
 
@@ -15,5 +25,102 @@ export async function getData(collectionName: string) {
       error
     );
     throw error;
+  }
+}
+
+export async function signUp(
+  userData: {
+    email: string;
+    fullname: string;
+    password: string;
+    role?: string;
+  },
+  callback: (response: { status: boolean; message: string }) => void
+) {
+  try {
+    // Query untuk memeriksa apakah email sudah ada
+    const q = query(
+      collection(firestore, "users"),
+      where("email", "==", userData.email)
+    );
+    const snapshot = await getDocs(q);
+
+    const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+    if (data.length > 0) {
+      // Jika email sudah ada
+      callback({ status: false, message: "Email already exists" });
+    } else {
+      // Hash password
+      userData.password = await bcrypt.hash(userData.password, 10);
+      userData.role = "member";
+
+      // Tambahkan data pengguna ke Firestore
+      await addDoc(collection(firestore, "users"), userData);
+      callback({ status: true, message: "Register Successfully" });
+    }
+  } catch (error) {
+    console.error("Error signing up:", error);
+    callback({ status: false, message: "Register Failed" });
+  }
+}
+
+export async function signIn(userData: { email: string }) {
+  try {
+    const q = query(
+      collection(firestore, "users"),
+      where("email", "==", userData.email)
+    );
+    const snapshot = await getDocs(q);
+
+    const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+    if (data.length > 0) {
+      return data[0];
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Error signing in:", error);
+    throw error;
+  }
+}
+
+export async function signWithGoogle(userData: any) {
+  try {
+    // Query untuk memeriksa apakah email sudah ada
+    const q = query(
+      collection(firestore, "users"),
+      where("email", "==", userData.email)
+    );
+    const snapshot = await getDocs(q);
+
+    const data: any = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    if (data.length > 0) {
+      userData.role = data[0].role;
+      // Jika email sudah ada, perbarui data pengguna
+      await updateDoc(doc(firestore, "users", data[0].id), userData);
+      return {
+        status: true,
+        message: "User updated successfully",
+        data: userData,
+      };
+    } else {
+      // Jika email belum ada, tambahkan data pengguna baru
+      userData.role = "member";
+      await addDoc(collection(firestore, "users"), userData);
+      return {
+        status: true,
+        message: "User registered successfully",
+        data: userData,
+      };
+    }
+  } catch (error) {
+    console.error("Error in signWithGoogle:", error);
+    return { status: false, message: "An error occurred during registration" };
   }
 }

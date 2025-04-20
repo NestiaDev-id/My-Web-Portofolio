b48b94c414ed2f0fcad6ef0d9594ac11fa081f64
NestiaDev-id <yohanesdevano90@gmail.com>
2025-04-21 03:38:13 +0700

    update

--- backend/next.config.ts
+++ backend/next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  dev: {
-    allowedDevOrigins: [
-      "http://localhost:3000",
-      "http://your-allowed-origin.com",
-    ],
-  },
+  
 };
 
 export default nextConfig;

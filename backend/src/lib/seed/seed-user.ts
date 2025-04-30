e6d60fa5dfbe12ef6f0a5aedd4fa4228d44c842c
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-01 01:40:12 +0700

    Update

--- backend/src/lib/seed/seed-user.ts
+++ backend/src/lib/seed/seed-user.ts
@@ -1,5 +1,5 @@
 // scripts/seed-user.ts
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 // import bcrypt from "bcryptjs";
 
 // const prisma = new PrismaClient();

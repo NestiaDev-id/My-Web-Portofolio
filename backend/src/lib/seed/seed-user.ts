99cb51fe9aeadce9a6530e2d4e13a6844cf4b653
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-01 01:39:27 +0700

    Update

--- backend/src/lib/seed/seed-user.ts
+++ backend/src/lib/seed/seed-user.ts
@@ -1,22 +1,22 @@
 // scripts/seed-user.ts
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-  const hash = await bcrypt.hash("12345678", 10);
+// async function main() {
+//   const hash = await bcrypt.hash("12345678", 10);
 
-  const user = await prisma.user.upsert({
-    where: { email: "budi@gmail.com" },
-    update: {},
-    create: {
-      email: "budi@gmail.com",
-      password: hash,
-    },
-  });
+//   const user = await prisma.user.upsert({
+//     where: { email: "budi@gmail.com" },
+//     update: {},
+//     create: {
+//       email: "budi@gmail.com",
+//       password: hash,
+//     },
+//   });
 
-  console.log("User created:", user);
-}
+//   console.log("User created:", user);
+// }
 
-main().finally(() => prisma.$disconnect());
+// main().finally(() => prisma.$disconnect());

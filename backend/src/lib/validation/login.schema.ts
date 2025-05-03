8085c63a5d241b1cf6f987b5e624937ba482d7f2
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-04 05:45:21 +0700

    6#remove unless program

--- backend/src/lib/validation/login.schema.ts
+++ backend/src/lib/validation/login.schema.ts
@@ -1,12 +1,17 @@
 import { z } from "zod";
 
-export default functionloginSchema = z.object({
+export const loginSchema = z.object({
   email: z
     .string()
-    .email()
-    .refine((email) => {
-      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-      return emailRegex.test(email) && email.endsWith("@gmail.com");
-    }),
-  password: z.string().min(6),
+    .email("Email tidak valid")
+    .refine(
+      (email) => {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return emailRegex.test(email) && email.endsWith("@gmail.com");
+      },
+      {
+        message: "Email harus menggunakan domain @gmail.com",
+      }
+    ),
+  password: z.string().min(6, "Password minimal 6 karakter"),
 });

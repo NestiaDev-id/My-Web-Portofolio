9a9b28882e3dc5f6b56a08e6806816dadedf4632
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-04 05:27:40 +0700

    3#remove unless program

--- backend/src/pages/api/blog/index.ts
+++ backend/src/pages/api/blog/index.ts
@@ -1,38 +1,38 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import clientPromise from "@/lib/mongodb/init";
-import { ObjectId } from "mongodb";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import clientPromise from "@/lib/mongodb/init";
+// import { ObjectId } from "mongodb";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "GET") {
-    const client = await clientPromise;
-    const db = client.db("portofolio");
-    const collection = db.collection("blog");
-    const blogs = await collection.find({}).toArray();
-    return res.status(200).json(blogs);
-  }
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method === "GET") {
+//     const client = await clientPromise;
+//     const db = client.db("portofolio");
+//     const collection = db.collection("blog");
+//     const blogs = await collection.find({}).toArray();
+//     return res.status(200).json(blogs);
+//   }
+// }
 
-export async function addBlog(req: NextApiRequest, res: NextApiResponse) {
-  const { title, content } = req.body;
-}
+// export async function addBlog(req: NextApiRequest, res: NextApiResponse) {
+//   const { title, content } = req.body;
+// }
 
-export async function editBlog(req: NextApiRequest, res: NextApiResponse) {
-  const { id, title, content } = req.body;
-  const client = await clientPromise;
-  const db = client.db("portofolio");
-  const collection = db.collection("blog");
+// export async function editBlog(req: NextApiRequest, res: NextApiResponse) {
+//   const { id, title, content } = req.body;
+//   const client = await clientPromise;
+//   const db = client.db("portofolio");
+//   const collection = db.collection("blog");
 
-  const result = await collection.updateOne(
-    { _id: new ObjectId(id) },
-    { $set: { title, content } }
-  );
+//   const result = await collection.updateOne(
+//     { _id: new ObjectId(id) },
+//     { $set: { title, content } }
+//   );
 
-  if (result.modifiedCount > 0) {
-    return res.status(200).json({ message: "Blog updated successfully" });
-  } else {
-    return res.status(500).json({ message: "Failed to update blog" });
-  }
-}
+//   if (result.modifiedCount > 0) {
+//     return res.status(200).json({ message: "Blog updated successfully" });
+//   } else {
+//     return res.status(500).json({ message: "Failed to update blog" });
+//   }
+// }

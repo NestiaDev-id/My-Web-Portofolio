2f1eaaf795344f02322937e88cb77b03a8346031
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-09 20:48:06 +0700

    Update project page

--- frontend/src/pages/ProjectPage.tsx
+++ frontend/src/pages/ProjectPage.tsx
@@ -11,7 +11,7 @@ const projects = [
     category: "Data Science", // Data Science (Machine Learning + Data Analysis)
     description:
       "A forecasting model that utilizes the Holt-Winter method, optimized using Genetic Algorithms, to predict time series data with improved accuracy.",
-    image: "https://picsum.photos/400/300?random=1",
+    image: "../../public/gambar2.png",
     status: "Completed",
     github:
       "https://github.com/NestiaDev-id/Optimization-Holt-winter-using-Genetic-algorithm",
@@ -22,7 +22,8 @@ const projects = [
     category: "Mobile Development", // Mobile App (Flutter)
     description:
       "A Dart/Flutter-based mobile app combining food ordering and ride-sharing features. Users can order food/drinks and schedule delivery while using Google Maps for location tracking.",
-    image: "https://picsum.photos/400/300?random=2",
+    image:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/035d20191314459.65c9cd731f289.jpg",
     status: "Completed",
     github: "https://github.com/NestiaDev-id/Wakul",
     demo: "#",
@@ -32,7 +33,8 @@ const projects = [
     category: "Data Science", // Machine Learning + Data Analysis
     description:
       "A machine learning-based movie recommendation system utilizing collaborative filtering. Integrated with a ViteJS frontend, this system provides personalized recommendations to users.",
-    image: "https://picsum.photos/400/300?random=3",
+    image:
+      "https://storage.googleapis.com/kaggle-datasets-images/3375918/5872805/e6c438e764799de9a90ae10bd32c51cc/dataset-cover.jpg?t=2023-06-08-06-58-37",
     status: "Ongoing",
     github: "#",
     demo: "#",
@@ -42,7 +44,8 @@ const projects = [
     category: "Data Science", // AI + Machine Learning
     description:
       "A natural language processing (NLP)-based chatbot designed to engage in meaningful conversations with users, providing intelligent responses and improving interaction.",
-    image: "https://picsum.photos/400/300?random=4",
+    image:
+      "https://www.guevariando.com/wp-content/uploads/2024/05/Mengenal-Chatbot-AI-dan-Cara-Membuatnya.png",
     status: "Development",
     github: "#",
     demo: "#",
@@ -52,7 +55,7 @@ const projects = [
     category: "Website Development", // Web Security
     description:
       "A robust authentication system providing security features such as CSRF, token-based authentication, refresh tokens, rate limiting, sliding sessions, IP blocking, and Argon2 encryption.",
-    image: "https://picsum.photos/400/300?random=5",
+    image: "../../public/gambar1.jpeg",
     status: "Completed",
     github: "https://github.com/NestiaDev-id/Auth-Defense-System",
     demo: "#",
@@ -62,7 +65,8 @@ const projects = [
     category: "Data Science", // AI/Computer Vision + ML
     description:
       "A smart CCTV system for vehicle detection using YOLO, CNN, and LSTM algorithms, optimized with genetic algorithms to improve accuracy in real-time monitoring.",
-    image: "https://picsum.photos/400/300?random=6",
+    image:
+      "https://www.researchgate.net/publication/326312085/figure/fig1/AS:963443049898007@1606714131967/Object-detection-with-a-pretrained-YOLO-model.jpg",
     status: "Development",
     github: "https://github.com/NestiaDev-id/Smart-CCTV",
     demo: "#",
@@ -72,7 +76,8 @@ const projects = [
     category: "Website Development", // Web Application
     description:
       "A point-of-sale (POS) system designed for retail businesses with two user roles: customers and cashiers. It allows seamless transaction handling and product management.",
-    image: "https://picsum.photos/400/300?random=7",
+    image:
+      "https://i0.wp.com/isellerdotblog.wpcomstaging.com/wp-content/uploads/2023/10/aben08381.jpg?resize=750%2C666&ssl=1",
     status: "Production",
     github: "https://scapa.vercel.app/",
     demo: "#",
@@ -82,7 +87,8 @@ const projects = [
     category: "Data Science", // Machine Learning + Data Analysis
     description:
       "A machine learning model designed to forecast weather conditions and predict rainfall in Indonesia. The system will be integrated into a website for easy access by users.",
-    image: "https://picsum.photos/400/300?random=8",
+    image:
+      "https://imgsrv2.voi.id/8rfakvmSRjzZGmEvTmJU-lYcFMH84vw30I4GBOGYwIU/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8yNDAzMjkvMjAyMjEyMzAxMDQ1LW1haW4uY3JvcHBlZF8xNjcyMzk0MzE3LmpwZw.jpg",
     status: "Ongoing",
     github: "https://github.com/NestiaDev-id/Map-Weathers",
     demo: "#",
@@ -92,7 +98,8 @@ const projects = [
     category: "Website Development", // Web Application
     description:
       "A messaging platform with multi-user support, similar to traditional chat apps, allowing users to communicate in real-time in a secure environment.",
-    image: "https://picsum.photos/400/300?random=9",
+    image:
+      "https://cdn5.vectorstock.com/i/1000x1000/91/94/mobile-phone-chat-message-notifications-chatting-vector-22379194.jpg",
     status: "Completed",
     github: "https://github.com/NestiaDev-id/ChatApp",
     demo: "#",
@@ -102,8 +109,9 @@ const projects = [
     category: "Website Development", // Web Application
     description:
       "A comprehensive online platform providing the complete Bible in various formats, allowing users to access scriptures easily and engage with spiritual content.",
-    image: "https://picsum.photos/400/300?random=10",
-    status: "Completed",
+    image:
+      "https://i.kickstarter.com/assets/041/147/393/ad16ab165b1a1271e076e21c81a864ca_original.png?anim=false&fit=cover&gravity=auto&height=873&origin=ugc&q=92&v=1685578998&width=1552&sig=HtnQQ%2FeE7AC%2FLhhK2TF76vSQ1er8QvhWqppb6Nw6Z7s%3D",
+    status: "Ongoing",
     github: "https://github.com/NestiaDev-id/Kitabku-Api",
     demo: "https://kitabku-api.vercel.app/",
   },
@@ -117,7 +125,7 @@ export default function ProjectPage() {
     filter === "All" ? projects : projects.filter((p) => p.category === filter);
 
   return (
-    <div className="container mx-auto mt-16 p-6 text-gray-200">
+    <div className="container mx-auto mt-16 p-6">
       <h2 className="text-4xl font-bold text-center mb-12">Proyek</h2>
 
       {/* Filter Buttons */}

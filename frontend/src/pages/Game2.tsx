161209f195feba8b80eb7822028269566b007add
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-12 03:35:57 +0700

    update collisions map

--- frontend/src/pages/Game2.tsx
+++ frontend/src/pages/Game2.tsx
@@ -5,6 +5,8 @@ import playerUpImage from "../assets/img/playerUp.png";
 import playerLeftImage from "../assets/img/playerLeft.png";
 import playerRightImage from "../assets/img/playerRight.png";
 import backgroundImage from "../assets/img/Pellet Town.png";
+import collisions from "@/assets/data/collisions";
+import { Boundary } from "./Boundary";
 
 const Game: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -43,6 +45,30 @@ const Game: React.FC = () => {
     const backgroundPosition = { x: -500, y: -400 };
     const speed = 3;
 
+    const collisionsMap = [];
+    const MAP_WIDTH = 70; // ganti sesuai lebar peta (jumlah kolom)
+
+    for (let i = 0; i < collisions.length; i += MAP_WIDTH) {
+      collisionsMap.push(collisions.slice(i, i + MAP_WIDTH));
+    }
+
+    const boundaries: Boundary[] = [];
+
+    collisionsMap.forEach((row, rowIndex) => {
+      row.forEach((symbol, colIndex) => {
+        if (symbol === 1025) {
+          boundaries.push(
+            new Boundary({
+              position: {
+                x: colIndex * Boundary.width,
+                y: rowIndex * Boundary.height,
+              },
+            })
+          );
+        }
+      });
+    });
+
     const player = new Sprite({
       position: {
         x: canvas.width / 2 - 24,
@@ -71,27 +97,166 @@ const Game: React.FC = () => {
 
     const update = () => {
       player.animate = false;
-
+      const movables = [backgroundPosition, boundaries]; // background dan batas bergerak
+      function rectangularCollision({
+        rect1,
+        rect2,
+      }: {
+        rect1: {
+          position: { x: number; y: number };
+          width: number;
+          height: number;
+        };
+        rect2: {
+          position: { x: number; y: number };
+          width: number;
+          height: number;
+        };
+      }) {
+        return (
+          rect1.position.x < rect2.position.x + rect2.width &&
+          rect1.position.x + rect1.width > rect2.position.x &&
+          rect1.position.y < rect2.position.y + rect2.height &&
+          rect1.position.y + rect1.height > rect2.position.y
+        );
+      }
       if (keys.w) {
-        backgroundPosition.y += speed;
-        player.image = player.sprites?.up || player.image;
-        player.animate = true;
-        lastKey = "w";
+        player.image = player.sprites?.up || player.image; // Menentukan sprite pemain untuk bergerak ke atas
+        player.animate = true; // Memulai animasi pemain
+        lastKey = "w"; // Menyimpan tombol terakhir yang ditekan
+
+        let canMove = true; // Flag untuk menentukan apakah pemain bisa bergerak
+        for (const boundary of boundaries) {
+          // Memeriksa tabrakan antara pemain dan boundary
+          if (
+            rectangularCollision({
+              rect1: {
+                position: { x: player.position.x, y: player.position.y }, // Posisi pemain
+                width: 48, // Lebar pemain
+                height: 48, // Tinggi pemain
+              },
+              rect2: {
+                position: {
+                  x: boundary.position.x + backgroundPosition.x,
+                  y: boundary.position.y + backgroundPosition.y + speed,
+                },
+                width: Boundary.width,
+                height: Boundary.height,
+              },
+            })
+          ) {
+            canMove = false; // Jika tabrakan terjadi, set flag canMove ke false
+            break; // Keluar dari loop karena sudah ada tabrakan
+          }
+        }
+        // Jika tidak ada tabrakan, maka background bergerak ke bawah (seolah-olah pemain bergerak ke atas)
+        if (canMove) backgroundPosition.y += speed;
       } else if (keys.a) {
-        backgroundPosition.x += speed;
-        player.image = player.sprites?.left || player.image;
-        player.animate = true;
-        lastKey = "a";
-      } else if (keys.s) {
-        backgroundPosition.y -= speed;
-        player.image = player.sprites?.down || player.image;
-        player.animate = true;
-        lastKey = "s";
-      } else if (keys.d) {
-        backgroundPosition.x -= speed;
-        player.image = player.sprites?.right || player.image;
-        player.animate = true;
-        lastKey = "d";
+        player.image = player.sprites?.left || player.image; // Menentukan sprite pemain untuk bergerak ke kiri
+        player.animate = true; // Memulai animasi pemain
+        lastKey = "a"; // Menyimpan tombol terakhir yang ditekan
+
+        let canMove = true; // Flag untuk menentukan apakah pemain bisa bergerak
+        for (const boundary of boundaries) {
+          // Memeriksa tabrakan antara pemain dan boundary
+          if (
+            rectangularCollision({
+              rect1: {
+                position: {
+                  x: player.position.x - speed, // Mengurangi posisi X pemain untuk bergerak ke kiri
+                  y: player.position.y,
+                },
+                width: 48, // Lebar pemain
+                height: 48, // Tinggi pemain
+              },
+              rect2: {
+                position: {
+                  x: boundary.position.x + backgroundPosition.x, // Posisi X boundary
+                  y: boundary.position.y + backgroundPosition.y, // Posisi Y boundary
+                },
+                width: Boundary.width, // Lebar boundary
+                height: Boundary.height, // Tinggi boundary
+              },
+            })
+          ) {
+            canMove = false; // Jika tabrakan terjadi, set flag canMove ke false
+            break; // Keluar dari loop karena sudah ada tabrakan
+          }
+        }
+        // Jika tidak ada tabrakan, maka background bergerak ke kanan (seolah-olah pemain bergerak ke kiri)
+        if (canMove) backgroundPosition.x += speed;
+      }
+      // Mengecek apakah tombol 'S' ditekan (bergerak ke bawah)
+      else if (keys.s) {
+        player.image = player.sprites?.down || player.image; // Menentukan sprite pemain untuk bergerak ke bawah
+        player.animate = true; // Memulai animasi pemain
+        lastKey = "s"; // Menyimpan tombol terakhir yang ditekan
+
+        let canMove = true; // Flag untuk menentukan apakah pemain bisa bergerak
+        for (const boundary of boundaries) {
+          // Memeriksa tabrakan antara pemain dan boundary
+          if (
+            rectangularCollision({
+              rect1: {
+                position: {
+                  x: player.position.x,
+                  y: player.position.y + speed, // Menambah posisi Y pemain untuk bergerak ke bawah
+                },
+                width: 48, // Lebar pemain
+                height: 48, // Tinggi pemain
+              },
+              rect2: {
+                position: {
+                  x: boundary.position.x + backgroundPosition.x, // Posisi X boundary
+                  y: boundary.position.y + backgroundPosition.y, // Posisi Y boundary
+                },
+                width: Boundary.width, // Lebar boundary
+                height: Boundary.height, // Tinggi boundary
+              },
+            })
+          ) {
+            canMove = false; // Jika tabrakan terjadi, set flag canMove ke false
+            break; // Keluar dari loop karena sudah ada tabrakan
+          }
+        }
+        // Jika tidak ada tabrakan, maka background bergerak ke atas (seolah-olah pemain bergerak ke bawah)
+        if (canMove) backgroundPosition.y -= speed;
+      }
+      // Mengecek apakah tombol 'D' ditekan (bergerak ke kanan)
+      else if (keys.d) {
+        player.image = player.sprites?.right || player.image; // Menentukan sprite pemain untuk bergerak ke kanan
+        player.animate = true; // Memulai animasi pemain
+        lastKey = "d"; // Menyimpan tombol terakhir yang ditekan
+
+        let canMove = true; // Flag untuk menentukan apakah pemain bisa bergerak
+        for (const boundary of boundaries) {
+          // Memeriksa tabrakan antara pemain dan boundary
+          if (
+            rectangularCollision({
+              rect1: {
+                position: {
+                  x: player.position.x + speed, // Menambah posisi X pemain untuk bergerak ke kanan
+                  y: player.position.y,
+                },
+                width: 48, // Lebar pemain
+                height: 48, // Tinggi pemain
+              },
+              rect2: {
+                position: {
+                  x: boundary.position.x + backgroundPosition.x, // Posisi X boundary
+                  y: boundary.position.y + backgroundPosition.y, // Posisi Y boundary
+                },
+                width: Boundary.width, // Lebar boundary
+                height: Boundary.height, // Tinggi boundary
+              },
+            })
+          ) {
+            canMove = false; // Jika tabrakan terjadi, set flag canMove ke false
+            break; // Keluar dari loop karena sudah ada tabrakan
+          }
+        }
+        // Jika tidak ada tabrakan, maka background bergerak ke kiri (seolah-olah pemain bergerak ke kanan)
+        if (canMove) backgroundPosition.x -= speed;
       }
 
       draw();
